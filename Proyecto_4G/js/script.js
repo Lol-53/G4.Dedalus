f5d47fcda9c039425143a130268618eb6d2cd542
amf1224 <amf1224@uma.es>
2025-03-13 23:45:59 +0100

    Primera version del Chat
    Falta ponerle que pueda crecer en altura el input, ponerle una altura máxima también y que se ajuste la visualización del chat a eso

--- Proyecto_4G/js/script.js
+++ Proyecto_4G/js/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let sidebar = document.getElementById("navBarCollapsed");
   let page = document.getElementById("page");
   let button = document.getElementById("menu-button");
+  let chat = document.getElementById("chat-body");
 
   // Verificamos si el sidebar está disponible
   if (!sidebar) {
@@ -9,14 +10,40 @@ document.addEventListener("DOMContentLoaded", function () {
     return;
   }
 
-  sidebar.style.height = page.offsetHeight + "px";
+  if(page.offsetHeight > window.innerHeight){
+    sidebar.style.height = page.offsetHeight + "px";
+  }else{
+    sidebar.style.height = window.offsetHeight + "px";
+  }
+
   if(window.innerWidth < 768 && (!sidebar.classList.contains("hidden"))){
     sidebar.classList.add("hidden");
     page.classList.add("expanded");
+    if(chat){
+      chat.style.setProperty("width", (window.innerWidth-250) + "px", "important");
+    }
+  }
 
+  if(chat && (window.innerWidth < 768 && (sidebar.classList.contains("hidden")))){
+    chat.style.setProperty("width", (window.innerWidth-50) + "px", "important");
+  }
+
+  if(window.innerWidth >= 768){
+    if(chat){
+      chat.style.setProperty("width", (window.innerWidth-250) + "px", "important");
+    }
+  }
+
+  if(window.innerWidth < 768){
+    button.classList.add("expanded");
+  }
+
+  if(chat){
+    chat.style.setProperty("max-height", (window.innerHeight-200) + "px", "important");
   }
 
   button.addEventListener("click", function(event){
+
     event.preventDefault();
     sidebar.style.display="flex";
 
@@ -25,7 +52,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
       // page.classList.add("hidden");
       sidebar.classList.remove("hidden");
+      if(chat){
+        chat.style.setProperty("width", (window.innerWidth-250) + "px", "important");
+      }
       page.classList.remove("expanded");
+
 
 
 
@@ -35,30 +66,58 @@ document.addEventListener("DOMContentLoaded", function () {
 
       page.classList.add("expanded");
       sidebar.classList.add("hidden");
+      if(chat){
+        chat.style.setProperty("width", (window.innerWidth-50) + "px", "important");
+      }
 
     }
   });
 
   // Detectamos el cambio de tamaño de la ventana para simular el colapso
   window.addEventListener("resize", function (event) {
+
     // Si el navbar está colapsado (según el tamaño de la pantalla)
 
     if (window.innerWidth < 768 ) {
       // Aplicamos la animación de deslizamiento
+      if(chat){
+        chat.style.setProperty("max-height", (window.innerHeight-200) + "px", "important");
+      }
       sidebar.classList.add("hidden");
       page.classList.add("expanded");
-      button.classList.add("expanded");
-      button.classList.remove("hidden");
-      page.style.width = "calc("+page.style.width+"+200px)"
-      sidebar.style.height = page.offsetHeight + "px";
+      if(!button.classList.contains("expanded")){
+        button.classList.add("expanded");
+        button.classList.remove("hidden");
+      }
+
+      page.style.width = "calc("+page.style.width+"px + 200px)"
+
+
+      if(page.offsetHeight > window.innerHeight){
+        sidebar.style.height = page.offsetHeight + "px";
+      }else{
+        sidebar.style.height = window.innerHeight + "px";
+      }
+      setTimeout(function(){if(chat){
+        chat.style.setProperty("width", (window.innerWidth-50) + "px", "important");
+      }},30);
 
     } else if (window.innerWidth >= 768) {
       // Si el tamaño de la pantalla es mayor y el navbar está oculto, lo mostramos
+      if(chat){
+        chat.style.setProperty("width", (window.innerWidth-250) + "px", "important");
+      }
+
       sidebar.classList.remove("hidden");
       page.classList.remove("expanded");
       button.classList.remove("expanded");
       button.classList.add("hidden");
-      sidebar.style.height = page.offsetHeight + "px";
+
+      if(page.offsetHeight > window.innerHeight){
+        sidebar.style.height = page.offsetHeight + "px";
+      }else{
+        sidebar.style.height = window.innerHeight + "px";
+      }
     }
   });
 
