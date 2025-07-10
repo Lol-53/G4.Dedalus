fae0c90bc7d5b5f197d8b0fe76a1c48c96db8f3d
elyez54 <eloygonzalez@uma.es>
2025-07-10 11:11:19 +0200

    cambio de toda la estructura de carpetas, limpieza

--- proyecto_conexion_flask_react/backend/cambioRutaDirectorio.py
+++
@@ -1,5 +0,0 @@
-import shutil
-
-
-def cambioRuta(origen_directorio: str, destino_directorio: str):
-    shutil.move(origen_directorio,destino_directorio)
--- proyecto_conexion_flask_react/src/pruebaComponent.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const MyComponent = () => {
-
-    // eslint-disable-next-line jsx-a11y/iframe-has-title
-    return <iframe src="/Prueba.html" width={"100%"} height={"100%"} />;
-};
-export default MyComponent;
